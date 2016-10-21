f23bfc148dd1c2fae1c0d75ed538e3f43ac00af5
RinaldoBuratti <rinaldoburatti@gmail.com>
2016-10-21 11:45:22 +0200

    error correction

--- 2016-10-14/workshop_01.py
+++ 2016-10-14/workshop_01.py
@@ -2,11 +2,23 @@ from pyplasm import *
 from larlib import *
 
 def buildStructure(beamSection, pillarSection, pillarDistances, beamDistances) :
+	"""
+	Create a simple parametric building structure in reinforced concrete
+    :param beamSize: given dimensions of beam section
+    :param pillarSize: given dimensions of pillar section
+    :param beamDistances: distances between axes of the pillars
+    :param pillarDistances: interstory heights
+    :return: HPC object
+    """
 
 	(bx, bz) = beamSection
 	(px, py) = pillarSection
 
-	#pillar creation
+	"""
+
+	pillar creation
+
+	"""
 
 	pillarX = []
 	for i in range(0, len(pillarDistances)):
@@ -20,7 +32,11 @@ def buildStructure(beamSection, pillarSection, pillarDistances, beamDistances) :
 	pillarSimple = PROD([QUOTE(pillarX), QUOTE(pillarY)])
 	pillarComplete = PROD([pillarSimple, Q(px)])
 
-	#beam creations
+	"""
+
+	beam creations
+
+	"""
 	beamX = []
 	for i in range(0, len(pillarDistances)):
 		if i == 0 or i == len(pillarDistances) - 1 :
@@ -36,15 +52,17 @@ def buildStructure(beamSection, pillarSection, pillarDistances, beamDistances) :
 	beamSimple = PROD([QUOTE(beamX), QUOTE(beamY)])
 	beamComplete = PROD([beamSimple, Q(bx)])
 
-	return STRUCT([pillarComplete, beamComplete])
+	structure = STRUCT([pillarComplete, beamComplete])
+	structure = R([1,2])(PI/2.0)(structure)
+	return R([1,3])(-PI/2.0)(structure)
 
+if __name__ == "__main__":
 
+	beamSection = (0.3, 0.3)
+	pillarSection = (0.3, 0.3)
+	pillarDistances = [2.5, 2, 3, 0.5, 1.5]
+	beamDistances = [2, 1, 0.5]
 
-beamSection = (0.3, 0.3)
-pillarSection = (0.3, 0.3)
-pillarDistances = [2.5, 2, 3, 0.5, 1.5]
-beamDistances = [2, 1, 0.5]
-
-structure = buildStructure(beamSection, pillarSection, pillarDistances, beamDistances)
+	structure = buildStructure(beamSection, pillarSection, pillarDistances, beamDistances)
 
 VIEW(structure)
